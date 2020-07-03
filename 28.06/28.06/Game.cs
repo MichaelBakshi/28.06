c5d2f5262c4eaaa887e230d81ad08fa2ac2f0466
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-07-03 09:32:02 +0300

    Add files via upload

---
+++ 28.06/28.06/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._06
+{
+    class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+
+        }
+
+        public Game(string _name, float _rating = 5.5f)
+        {
+            this._name = _name;
+            this._rating = _rating;
+        }
+
+        public Game(string _name,  int _numberOfPlayers, float _rating, string _originCountry)
+        {
+            this._name = _name;
+            this._numberOfPlayers = _numberOfPlayers;
+            this._rating = _rating;
+            this._originCountry = _originCountry;
+        }
+
+        public Game(string _name, string _originCountry)
+        {
+            this._name = _name;
+            this._originCountry = _originCountry;
+        }
+
+        public int TellMeHowManyPlayers ()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} _name : {_name} _number Of Players   : {_numberOfPlayers} " +
+                $" _rating : {_rating } _originCountry : {_originCountry}";
+        }
+
+
+    }
+}
---
+++ 28.06/28.06/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._06
+{ 
+    class Program
+    {
+        static void PrintGame(Game g)
+        {
+            Console.WriteLine(g);
+            int number = g.TellMeHowManyPlayers();
+            Console.WriteLine(number);
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            Game g1 = new Game()
+            {
+                _name = "poker",
+                _numberOfPlayers = 4,
+                _rating = 7.8f,
+                _originCountry = "USA"
+
+            };
+            PrintGame(g1);
+
+            Game g2 = new Game("chess", 8.5f)
+            {
+                _numberOfPlayers = 2,
+                _originCountry = "China"
+            };
+
+            PrintGame(g2);
+
+            Game g3 = new Game("football", 22, 4.5f, "England");
+            PrintGame(g3);
+
+
+            Game g4 = new Game("sudoku", "India")
+            {
+                _rating = 7.1f,
+                _numberOfPlayers =1
+            };
+            PrintGame(g4);
+
+
+
+            
+        }
+
+
+    }
+}
